6fd78f81cbff069f024eccec6feb2de84865b64c
Johnnie Munger <johnnietheblack@gmail.com>
2013-12-11 22:50:53 -0800

    moved init to scaffolding

--- src/core/scaffold.js
+++ src/core/scaffold.js
@@ -103,6 +103,29 @@
 						? BBB.create(target) : target;
 				BBB.copy(targetNode,source);
 			},
+
+			/**
+			 * Initializes the entire BBB framework.
+			 **/
+			init:function(options) {
+			
+				var settings = {
+					elements:true
+				}
+				
+				BBB.copy(settings,options,true);
+			
+				// initialize UI elements
+				if(settings.elements && BBB.ui.elements) {
+					if(BBB.dom.isReady) {
+						BBB.ui.elements.parse();
+					}else{
+						BBB.events.subscribe('dom.ready',function() {
+							BBB.ui.elements.parse();
+						});
+					}
+				}
+			},
 			
 			/**
 			 * Custom console logging.
